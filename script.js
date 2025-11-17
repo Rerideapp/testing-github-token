363d32dd96e917fdd926489a0dc72107b38b3fe2
Rerideapp <abdabarber98@gmail.com>
2025-11-17 01:42:16 -0600

    Add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Application initialized');
+
+function handleCTA() {
+    alert('Welcome! This app was created with CPOS.');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Ready to build amazing things!');
+});
